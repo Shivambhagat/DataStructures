29fb9523f4dac2e3efb52a72911a0ae3e9e05124
Shivambhagat <56788081+Shivambhagat@users.noreply.github.com>
2022-10-03 00:30:46 +0530

    Update rolling_hash.py

--- rolling_hash.py
+++ rolling_hash.py
@@ -11,16 +11,26 @@ class ComputeHash:
         hash_value = 0
 
         for i in range(n):
-            c = ord(s[i]) - 65 + 1
+            c = ord(s[i]) - 97 + 1
             hash_value = (hash_value + c * p_pow) % self.mod
             self.hash[i] = hash_value
             self.inv_mod[i] = pow(p_pow, self.mod - 2, self.mod)
             p_pow = (p_pow * self.p) % self.mod
 
     def get_hash(self, l, r):
-
         if l == 0:
             return self.hash[r]
-
         window = (self.hash[r] - self.hash[l - 1]) % self.mod
         return (window * self.inv_mod[l]) % self.mod
+
+
+class Hash:
+
+    def __init__(self, s):
+        self.hash_a = ComputeHash(s, 31, 10 ** 9 + 9)
+        self.hash_b = ComputeHash(s, 37, 10 ** 9 + 7)
+
+    def __getitem__(self, R: tuple) -> tuple:
+        h1 = self.hash_a.get_hash(R[0], R[1])
+        h2 = self.hash_b.get_hash(R[0], R[1])
+        return h1, h2
