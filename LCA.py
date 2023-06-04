d922e5ea273cc8c6cdea30754a790bf317674aa9
Shivambhagat <56788081+Shivambhagat@users.noreply.github.com>
2023-06-05 00:51:22 +0530

    Update LCA.py

--- LCA.py
+++ LCA.py
@@ -1,4 +1,17 @@
-# import RMQ in here
+class RangeQuery:
+    def __init__(self, data, func=min):
+        self.func = func
+        self._data = _data = [list(data)]
+        i, n = 1, len(_data[0])
+        while 2 * i <= n:
+            prev = _data[-1]
+            _data.append([func(prev[j], prev[j + i]) for j in range(n - 2 * i + 1)])
+            i <<= 1
+
+    def query(self, begin, end):
+        depth = (end - begin).bit_length() - 1
+        return self.func(self._data[depth][begin], self._data[depth][end - (1 << depth)])
+
 
 class LCA:
     def __init__(self, root, graph):
