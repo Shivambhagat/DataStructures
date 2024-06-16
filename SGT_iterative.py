e6ba8f601cb197c2531a803bef5951d9959adfd0
Shivam Bhagat <shivampkrr@gmail.com>
2024-06-16 13:44:53 +0530

    adding iterative segment tree

---
+++ SGT_iterative.py
@@ -0,0 +1,38 @@
+class SegmentTree:
+    def __init__(self, array, func=max):
+        self.n = len(array)
+        self.size = 2**(int(log2(self.n-1))+1) if self.n != 1 else 1
+        self.func = func
+        self.default = 0 if self.func != min else inf
+        self.data = [self.default] * (2 * self.size)
+        self.process(array)
+    
+    def process(self, array):
+        self.data[self.size : self.size+self.n] = array
+        for i in range(self.size-1, -1, -1):
+            self.data[i] = self.func(self.data[2*i], self.data[2*i+1])
+    
+    def query(self, alpha, omega):
+        if alpha == omega:
+            return self.data[alpha + self.size]
+        res = self.default
+        alpha += self.size
+        omega += self.size + 1
+        while alpha < omega:
+            if alpha & 1:
+                res = self.func(res, self.data[alpha])
+                alpha += 1
+            if omega & 1:
+                omega -= 1
+                res = self.func(res, self.data[omega])
+            alpha >>= 1
+            omega >>= 1
+        return res
+
+    def update(self, index, value):
+        index += self.size
+        self.data[index] = value
+        index >>= 1
+        while index:
+            self.data[index] = self.func(self.data[2*index], self.data[2*index+1])
+            index >>= 1
